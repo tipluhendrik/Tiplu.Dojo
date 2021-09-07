321fed51db840dcc2fa3dd21e709b1ee5fc050e1
tiplucriss <68321512+tiplucriss@users.noreply.github.com>
2021-09-07 15:35:39 +0200

    Update UnitTest1.cs

--- Dojo.Tests/UnitTest1.cs
+++ Dojo.Tests/UnitTest1.cs
@@ -7,22 +7,22 @@ namespace Dojo.Tests
     public class UnitTest1
     {
         [Theory]
-        [InlineData(0,"FizzBuzz")]
-        [InlineData(1,"1")]
-        [InlineData(2,"2")]
-        [InlineData(3,"Fizz")]
-        [InlineData(4,"4")]
-        [InlineData(5,"Buzz")]
-        [InlineData(6,"Fizz")]
-        [InlineData(7,"7")]
-        [InlineData(8,"8")]
-        [InlineData(9,"Fizz")]
-        [InlineData(10,"Buzz")]
-        [InlineData(11,"11")]
-        [InlineData(12,"Fizz")]
-        [InlineData(13,"13")]
-        [InlineData(14,"14")]
-        [InlineData(15,"FizzBuzz")]
+        [InlineData(0, "FizzBuzz")]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        [InlineData(4, "4")]
+        [InlineData(5, "Buzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(7, "7")]
+        [InlineData(8, "8")]
+        [InlineData(9, "Fizz")]
+        [InlineData(10, "Buzz")]
+        [InlineData(11, "11")]
+        [InlineData(12, "Fizz")]
+        [InlineData(13, "13")]
+        [InlineData(14, "14")]
+        [InlineData(15, "FizzBuzz")]
         public void Test1(int number, string expectedResult)
         {
             var fizzBuzzer = new FizzBuzzer();
@@ -36,8 +36,24 @@ namespace Dojo.Tests
         {
             public string FizzBuzz(int number)
             {
-                StringBuilder
-                return null;
+                var sb = new StringBuilder(1, 8);
+
+                if (number % 3 == 0)
+                {
+                    sb.Append("Fizz");
+                }
+
+                if (number % 5 == 0)
+                {
+                    sb.Append("Buzz");
+                }
+
+                if (sb.Length == 0)
+                {
+                    sb.Append(number);
+                }
+
+                return sb.ToString();
             }
         }
     }
